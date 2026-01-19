5af9a40b36b3135148201959cb77199e7b93c165
ThibaultSS <thibault.soumoy@student.kdg.be>
2026-01-19 11:32:34 +0100

    aanpassingen controller score

--- app/Http/Controllers/CalculateAiScore.php
+++ app/Http/Controllers/CalculateAiScore.php
@@ -12,14 +12,19 @@ class CalculateAiScore extends Controller
     public function calculateScore(Request $request)
     {
         $score = (int) $request->input('score');
-        Aiornot::create([
-            'student_id' => session('student_id'),
-            'time' => 0,
-            'score' => $score,
-            'total_score' => $score,
-        ]);
-        Student::where('id', session('student_id'))->increment('total_score', $score);
-        return view("game.hexa-guess");   
+        $score = min($score, 10);
+        if(Aiornot::where('student_id', session('student_id'))->exists()){
+            return view("game.hexa-guess"); 
+        }
+        else{
+            Aiornot::create([
+                'student_id' => session('student_id'),
+                'time' => 0,
+                'score' => $score,
+                'total_score' => $score(max),
+            ]);        
+            return view("game.hexa-guess");     
+        }
 
     } 
 }
--- app/Http/Controllers/CalculateCometScore.php
+++ app/Http/Controllers/CalculateCometScore.php
@@ -12,16 +12,20 @@ class CalculateCometScore extends Controller
     public function calculateScore(Request $request)
     {
         $score = (int) $request->input('score');
+        $score = min($score, 50);
         $total_score = (int)($score / 5);
-        Typeracer::create([
-            'student_id' => session('student_id'),
-            'time' => 0,
-            'score' => $score,
-            'total_score' => $total_score,
-        ]);
-                
-        Student::where('id', session('student_id'))->increment('total_score', $total_score);
-        return view("game.ai-or-not");   
+        if(Typeracer::where('student_id', session('student_id'))->exists()){
+            return view("game.ai-or-not"); 
+        }
+        else{
+            Typeracer::create([
+                'student_id' => session('student_id'),
+                'time' => 0,
+                'score' => $score,
+                'total_score' => $total_score,
+            ]);
+            return view("game.ai-or-not");          
+        }
 
     }
 }
